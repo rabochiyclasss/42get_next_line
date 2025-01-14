471ffae41a49e935a0a87ba6d7055bf3d55eaec7
rabochiyclasss <20budkoivan02@gmail.com>
2025-01-14 22:32:21 +0100

    fisrt commit

---
+++ get_next_line.c
@@ -0,0 +1,84 @@
+#include "get_next_line.h"
+
+
+static void	read_from_fd(int fd, char **pointing, char *buff)
+{
+	int		bytes_read;
+	char 	*temp;
+
+	bytes_read = 1;
+	while (bytes_read > 0 && *pointing)
+	{
+		bytes_read = read(fd, buff, BUFFER_SIZE);
+		if (bytes_read == -1)
+		{
+			free(*pointing);
+			*pointing = NULL;
+			break ;
+		}
+		buff[bytes_read] = '\0';
+		temp = *pointing;
+		*pointing = ft_strjoin(temp, buff);
+		free(temp);
+		if (ft_strchr(buff, '\n'))
+			break ;
+	}
+	free(buff);
+}
+
+
+static void	get_line(char *pointing, char **line)
+{
+	char	*temp;
+	size_t	index;
+	int		next_line;
+	
+	index = 0;
+	while (pointing[index] && pointing[index] != '\n')
+		index++;
+	temp = ft_substr(pointing, 0, index + (pointing[index] == '\n'));
+	if (temp)
+	{
+		*line = (char *)malloc(sizeof(char) * ft_strlen(temp) + 1);
+		if (!*line)
+		{
+			free(temp);
+			return ;
+		}
+	}
+	ft_strlcpy(*line, temp, ft_strlen(temp) + 1);
+	free(temp);
+}
+
+static void	next_pointer(char **pointing, char *line)
+{
+	char	*temp;
+
+	temp = NULL;
+	if (*pointing || line)
+		temp = ft_strdup(*pointing + ft_strlen(line));
+	free(*pointing);
+	*pointing = temp;
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*pointing;
+	char		*line;
+	char 		*buff;
+
+	line = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if(!pointing)
+	{
+		pointing = ft_strdup("");// зачем так если можно просто pointing = NULL;
+	}
+	buff = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buff)
+		return (free(pointing), pointing = NULL, NULL);
+	read_from_fd(fd, &pointing, buff);
+	get_line(pointing, &line);
+	next_pointer(&pointing, line);
+	return (line);
+}
---
+++ get_next_line_utils.c
@@ -0,0 +1,126 @@
+#include "get_next_line.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	src_len;
+
+	if (!dst || !src)
+		return (0);
+	i = 0;
+	src_len = ft_strlen(src);
+	if (dstsize > 0)
+	{
+		while (src[i] && i < dstsize - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
+	return (src_len);
+}
+
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+	size_t	i;
+	size_t	size;
+
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) <= start)
+		len = 0;
+	if (ft_strlen(s) - start < len)
+		size = ft_strlen(s) - start;
+	else
+		size = len;
+	ptr = (char *)malloc((size + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < len && s[start + i])
+	{
+		ptr[i] = s[start + i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	if (c < 0 || c > 255)
+		return ((char *)s);
+	while (*s)
+	{
+		if (*s == c)
+			return ((char *)s);
+		s++;
+	}
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ptr;
+	size_t	i;
+	size_t	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	ptr = (char *)malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+	{
+		ptr[i + j] = s2[j];
+		j++;
+	}
+	ptr[i + j] = '\0';
+	return (ptr);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*ptr;
+	int		i;
+
+	if (!s1)
+		return (NULL);
+	ptr = (char *)malloc((ft_strlen(s1) + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
